b486c6188c70a2ffdfdefd0570db74320261ca11
xucui <xucui@microstrategy.com>
2019-02-26 14:57:00 +0800

    Init project sketch

---
+++ cmake/FindGLog.cmake
@@ -0,0 +1,14 @@
+find_path(LIBGLOG_INCLUDE_DIR glog/logging.h)
+find_library(LIBGLOG_LIBRARY glog)
+
+set(LIBGLOG_LIBRARIES ${LIBGLOG_LIBRARY})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+    LIBGLOG DEFAULT_MSG LIBGLOG_INCLUDE_DIR LIBGLOG_LIBRARIES)
+
+mark_as_advanced(LIBGLOG_INCLUDE_DIR LIBGLOG_LIBRARIES LIBGLOG_FOUND)
+
+if(LIBGLOG_FOUND AND NOT LIBGLOG_FIND_QUIETLY)
+    message(STATUS "LIBGLOG: ${LIBGLOG_INCLUDE_DIR}")
+endif()
